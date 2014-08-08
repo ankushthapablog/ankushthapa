17c9592234cf81a08ee5900ed67301476573be38
root <root@ip-172-31-19-241.us-west-2.compute.internal>
2014-08-08 19:00:40 +0000

    changed server side url

--- ankushthapa/urls.py
+++ ankushthapa/urls.py
@@ -5,7 +5,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', include('homepage.urls')),
-    url(r'^dev$', 'homepage.views.dev'),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'homepage.views.dev'),
+    #url(r'^admin/', include(admin.site.urls)),
 )
